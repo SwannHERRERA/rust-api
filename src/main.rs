4b635903a4872cc8afe2827fee0caa753313d364
Swann <swann@devloup.dev>
2020-08-08 00:14:52 +0200

    test avant dodo

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,33 @@
-extern crate chrono;
+#![feature(proc_macro_hygiene, decl_macro)]
 
 #[macro_use]
 extern crate diesel;
-
+extern crate chrono;
 extern crate dotenv;
+extern crate r2d2;
+extern crate r2d2_diesel;
+extern crate rocket;
+extern crate serde_derive;
+extern crate serde_json;
 
-use diesel::pg::PgConnection;
-use diesel::prelude::*;
 use dotenv::dotenv;
 use std::env;
 
+mod db;
 mod models;
 mod schema;
+mod static_files;
 
-fn main() {
+fn rocket() -> rocket::Rocket {
     dotenv().ok();
     let database_url = env::var("DATABASE_URL").expect("set DATABASE_URL");
-    let conn = PgConnection::establish(&database_url).unwrap();
 
-    let chapter = models::NewChapter {
-        title: String::from("Swann Herrera"),
-        author: String::from("Le début de l'énigme"),
-        published: true,
-    };
+    let pool = db::init_pool(database_url);
+    rocket::ignite()
+        .manage(pool)
+        .mount("/", routes![static_files::all, static_files::index])
+}
 
-    if models::Chapters::insert(chapter, &conn) {
-        println!("success");
-    } else {
-        println!("fail");
-    }
+fn main() {
+    rocket().launch();
 }
